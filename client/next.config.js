7cbcd9daefa4efe34181628bcca8f6493ee3ae64
vikyw89 <vikyw89@gmail.com>
2023-05-26 15:41:41 +0800

    updated next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? undefined,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : undefined,
     images: {
-        unoptimized: true,
+        unoptimized: true
     },
-    trailingSlash:true
+    trailingSlash: true
 }
 
 module.exports = nextConfig
