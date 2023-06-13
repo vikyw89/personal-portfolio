1daf170169639489062e580ba4a52381dc088156
vikyw89 <vikyw89@gmail.com>
2023-06-13 14:04:33 +0800

    installed framer motion

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="dracula">
       <body className={inter.className}>
-        <div className='fixed top-0 left-0 right-0 bottom-0 bg-neutral text-neutral-content flex flex-column overflow-x-hidden'>
+        <div className='fixed top-0 left-0 right-0 bottom-0 bg-neutral text-neutral-content overflow-x-hidden'>
           {children}
         </div>
       </body>
