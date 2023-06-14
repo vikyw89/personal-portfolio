767e709e7c8030da259647adbb402ada0f4c67b6
vikyw89 <vikyw89@gmail.com>
2023-06-14 17:43:21 +0800

    adjusted animation

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,37 +7,50 @@ export default function Home() {
 
   return (
     <main className="content flex flex-col w-full min-h-full items-center relative">
-      <div className='background fixed z-0'>
-
+      <div className='background fixed -z-10 top-0 right-0 bottom-0 left-0 bg-neutral'>
+        <motion.div
+          animate={{
+            x: [-100, 1000],
+            opacity: [100, 100]
+          }}
+          transition={{
+            duration: 10,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatDelay: 2
+          }}
+          className='text-9xl font-bold opacity-0 text-neutral-content'
+        >
+          HELLO
+        </motion.div>
       </div>
       <div className='focus fixed z-10'>
-
+        <motion.div
+          whileHover={{ scale: 6 }}>
+          Hello I&apos;m Viky
+        </motion.div>
       </div>
       <div className='foreground fixed z-20'>
-
+        <motion.div
+          animate={{
+            y: [-1000, 1000],
+            opacity: [100, 100]
+          }}
+          transition={{
+            duration: 10,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatDelay: 2
+          }}
+          className='text-9xl font-bold opacity-0 [writing-mode:vertical-lr] text-neutral-focus'
+        >
+          HELLO
+        </motion.div>
       </div>
-      <motion.div
-        whileHover={{ scale: 6 }}>
-        Hello I&apos;m Viky
-      </motion.div>
-      <motion.div
-        animate={{
-          y: [0,1000],
-          opacity:[100,10]
-        }}
-        transition={{
-          duration: 10,
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatDelay: 2
-        }}
-        className='[writing-mode:vertical-lr] text-5xl font-bold'
-      >
-        HELLO !
-      </motion.div>
+      {/* 
       <div>
         Works
-      </div>
+      </div> */}
       <ProjectCardContainer />
     </main>
   )
