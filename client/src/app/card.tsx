6041c0230e317b9ba286a56c09ba574553d81993
vikyw89 <vikyw89@gmail.com>
2023-06-29 19:22:55 +0800

    readjusted cards animation

--- client/src/app/card.tsx
+++ client/src/app/card.tsx
@@ -25,46 +25,69 @@ export const Card = ({ props }: CardProps) => {
             video.current.pause()
         }
     }, [isInView])
-
+    const viewportEnterHandler = () => {
+        // video.current.play()
+    }
+    const viewportLeaveHandler = () => {
+        // video.current.pause()
+    }
     return (
-        <motion.div className="aspect-auto w-full z-20 grid text-base-content">
-            <div className="relative w-full">
-                <motion.video preload="auto" className="aspect-auto w-full shadow-2xl" muted ref={video}
-                >
-                    <source src={props.videoSrc} type="video/webm" />
-                </motion.video>
-                <motion.div className="links inset-0 absolute flex flex-col items-end justify-between font-extrabold"
-                    initial={{
-                        x: 30
-                    }}
-                    whileInView={{
-                        x: 10
-                    }}
-                    transition={{
-                        type: 'spring',
-                        duration:1
-                    }}
-                >
-                    <a className="bg-info btn btn-square rounded-r-none bg-opacity-60 backdrop-blur-md flex-auto" href={props.liveLink}>
-                        <span className="-rotate-90 text-info-content">
-                            Live
-                        </span>
-                    </a>
-                    <a className="bg-info  btn btn-square rounded-r-none bg-opacity-60 backdrop-blur-md flex-auto" href={props.repoLink}>
-                        <span className="-rotate-90 text-info-content">
-                            Repo
-                        </span>
-                    </a>
-                </motion.div>
-            </div>
-            <div className="description p-2">
-                <div className="title font-extrabold">
-                    {props.title}
+        <>
+            <motion.div className="aspect-auto w-full z-20 grid text-base-content"
+                initial={{
+                    opacity: 0,
+                    scale: 0.8,
+                }}
+                whileInView={{
+                    opacity: 1,
+                    scale: 1,
+                }}
+                exit={{
+                    opacity: 0,
+                    scale: 0,
+                }}
+                onViewportEnter={viewportEnterHandler}
+                onViewportLeave={viewportLeaveHandler}
+            >
+                <div className="relative w-full">
+                    <motion.video preload="auto" className="aspect-auto w-full shadow-2xl" muted ref={video}
+                    >
+                        <source src={props.videoSrc} type="video/webm" />
+                    </motion.video>
+                    <motion.div className="links inset-0 absolute flex flex-col items-end justify-between font-extrabold"
+                        initial={{
+                            x: 30
+                        }}
+                        whileInView={{
+                            x: 10
+                        }}
+                        transition={{
+                            type: 'spring',
+                            duration: 1
+                        }}
+
+                    >
+                        <a className="bg-info btn btn-square rounded-r-none bg-opacity-60 backdrop-blur-md flex-auto" href={props.liveLink}>
+                            <span className="-rotate-90 text-info-content">
+                                Live
+                            </span>
+                        </a>
+                        <a className="bg-info  btn btn-square rounded-r-none bg-opacity-60 backdrop-blur-md flex-auto" href={props.repoLink}>
+                            <span className="-rotate-90 text-info-content">
+                                Repo
+                            </span>
+                        </a>
+                    </motion.div>
                 </div>
-                <div>
-                    {props.description}
+                <div className="description p-2">
+                    <div className="title font-extrabold">
+                        {props.title}
+                    </div>
+                    <div>
+                        {props.description}
+                    </div>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </>
     )
 }
