632d227e72578b84465b1dd78a4c882078acde5e
vikyw89 <vikyw89@gmail.com>
2023-07-11 22:51:43 +0800

    improved background with softer text

--- client/src/app/background.tsx
+++ client/src/app/background.tsx
@@ -7,9 +7,12 @@ export const Background = () => {
     const pathName = usePathname()
 
     return (
-        <div className='background fixed z-10 inset-0 opacity-20 text-base-content'>
+        <div className='background fixed z-10 inset-0 opacity-[0.05] text-base-content'>
             <ReactFitty className="absolute right-0 [writing-mode:vertical-lr] h-14 select-none" maxSize={200}>
                 {pathName}
+            </ReactFitty>
+            <ReactFitty className="absolute bottom-0">
+                viky
             </ReactFitty>
         </div>
     )
