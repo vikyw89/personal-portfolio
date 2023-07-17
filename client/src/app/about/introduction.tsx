3faa2e227f95a2b992517c108c7bb60ad5c1d9ce
vikyw89 <vikyw89@gmail.com>
2023-07-17 18:59:33 +0800

    refactor: moved content into variables

--- client/src/app/about/introduction.tsx
+++ client/src/app/about/introduction.tsx
@@ -6,14 +6,63 @@ import Link from 'next/link'
 import Marquee from 'react-fast-marquee'
 import { AnimatedMe } from './animatedMe'
 
-const backStory = 'someone with a mixed experience in architectural engineering and web developments'
-const links = {
-  linkedIn: 'https://www.linkedin.com/in/vikyweb/',
-  github: 'https://github.com/vikyw89',
-  gmail: 'vikyw89@gmail.com',
-  whatsApp: `https://wa.me/+6281234001700?text=${encodeURIComponent('I found you from your portfolio site')}`,
-  repo: 'https://github.com/vikyw89/personal-portfolio',
-  resume: 'https://firebasestorage.googleapis.com/v0/b/portfolio-b5ad9.appspot.com/o/030723_Daniel_Viky_Wahjoedin_Resume.pdf?alt=media&token=5e3a5a3a-6fb4-4c84-a1c8-81a18e8da169'
+const content = {
+  links: {
+    linkedIn: 'https://www.linkedin.com/in/vikyweb/',
+    github: 'https://github.com/vikyw89',
+    gmail: 'vikyw89@gmail.com',
+    whatsApp: `https://wa.me/+6281234001700?text=${encodeURIComponent('I found you from your portfolio site')}`,
+    repo: 'https://github.com/vikyw89/personal-portfolio',
+    resume: 'https://firebasestorage.googleapis.com/v0/b/portfolio-b5ad9.appspot.com/o/030723_Daniel_Viky_Wahjoedin_Resume.pdf?alt=media&token=5e3a5a3a-6fb4-4c84-a1c8-81a18e8da169'
+  },
+  backstory: 'someone with a mixed experience in architectural engineering and web developments',
+  bio: {
+    name: 'viky',
+    level: '??/??',
+    class: 'web developer',
+    race: 'human',
+    origin: 'earth'
+  },
+  abilities: [
+    'self learner',
+    'solution finder',
+    'job exp bonus'
+  ],
+  technicalExperience: {
+    languages: [
+      'Javascript', 'Typescript', 'Python', 'HTML5', 'CSS3', 'SQL', 'bash', 'C'
+    ],
+    frameworks: [
+      'NextJS', 'Flask', 'Express'
+    ],
+    tooling: [
+      'webpack', 'Vite', 'prettier', 'eslint', 'typedoc', 'docker', 'infisical', 'docker'
+    ],
+    libraries: [
+      'framer-motion', 'threeJS', 'swiper', 'bootstrap', 'material-ui', 'tailwindCSS', 'daisy-ui', 'react-router', 'redux', 'zustand'
+    ],
+    testing: [
+      'jest', 'vitest'
+    ],
+    'ci/cd': [
+      'github action'
+    ],
+    databases: [
+      'postgreSQL', 'firestore', 'mongodb'
+    ],
+    orm: [
+      'prisma'
+    ],
+    hosting: [
+      'vercel', 'github page', 'firebase', 'fly.io', 'netlify', 'cyclic.sh', 'railway.app'
+    ]
+  },
+  likes: {
+    OS: ['fedora', 'android'],
+    genre: ['sci/fi', 'adventure', 'fantasy'],
+    music: ['pop', 'rock', 'ost'],
+    person: ['Irene']
+  }
 }
 
 const itemVariants: Variants = {
@@ -45,26 +94,18 @@ export const IntroductionComponent = () => {
       animate='intro'
     >
       <motion.div className="row-[1/2] p-6 col-[1/-1] max-w-[50vw] z-50 relative pb-10 sm:p-10 intro">
-        <motion.h1 className="font-extrabold" variants={itemVariants}>
-          .name
-        </motion.h1>
-        <motion.h1 className="pl-5" variants={itemVariants}>
-          viky
-        </motion.h1>
-        <motion.h1 className="font-extrabold" variants={itemVariants}>
-          .level
-        </motion.h1>
-        <motion.h1 className="pl-5" variants={itemVariants}>
-          ??/??
-        </motion.h1>
-        <motion.h1 className="font-extrabold" variants={itemVariants}>
-          .class
-        </motion.h1>
-        <motion.div className="grid pl-5" variants={itemVariants}>
-          <motion.h1 variants={itemVariants}>
-            web developer
-          </motion.h1>
-        </motion.div>
+        {Object.entries(content.bio).map((v, i) => {
+          return (
+            <div key={i}>
+              <motion.h1 className="font-extrabold" variants={itemVariants}>
+                .{v[0]}
+              </motion.h1>
+              <motion.h1 className="pl-5" variants={itemVariants}>
+                {v[1]}
+              </motion.h1>
+            </div>
+          )
+        })}
       </motion.div>
       <Marquee autoFill speed={10} className="h-6 row-[2/3] col-[1/-1] absolute -top-6 cursor-default">
         <motion.span className="font-extrabold text-primary z-10"
@@ -116,84 +157,49 @@ export const IntroductionComponent = () => {
         <motion.h1 className="font-extrabold" variants={itemVariants}>
           .abilities
         </motion.h1>
-        <motion.div className="grid pl-5" variants={itemVariants}>
-          <motion.h1 variants={itemVariants}>
-            self learner
-          </motion.h1 >
-          <motion.h1 variants={itemVariants}>
-            docs exploration
-          </motion.h1 >
-          <motion.h1 variants={itemVariants}>
-            skills exp boost
-          </motion.h1 >
-        </motion.div>
+        {content.abilities.map((v, i) => {
+          return (
+            <motion.h1 key={i} variants={itemVariants} className='pl-5'>
+              {v}
+            </motion.h1 >
+          )
+        })}
         <motion.h1 className="font-extrabold hidden sm:block" variants={itemVariants}>
           .technicalExperiences
         </motion.h1>
         <motion.div className="hidden sm:grid pl-5">
-          <motion.h1 variants={itemVariants}>
-            languages: Javascript, Typescript, Python, HTML5, CSS3, SQL, Python, C
-          </motion.h1 >
-          <motion.h1 variants={itemVariants}>
-            databases: postgresql, sqlite, firestore, cloudinary, mongodb
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            ci/cd: github action
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            orm: prisma
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            frameworks: Next.js, Express.js, Flask
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            ui-frameworks: bootstrap, TailwindCSS, Material-ui, daisy-UI, framer-motion, three.js
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            state-management: redux, zustand
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            secret-management: infisical
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            tooling: eslint, Typedoc, vscode, prettier,
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            builder: vite, webpack
-          </motion.h1>
+          {Object.entries(content.technicalExperience).map((v, i) => {
+            return (
+              <motion.div key={i} variants={itemVariants}>
+                <span className='font-bold'>{v[0]}:</span>{v[1].map((v, i) => <span key={i}>{` ${v} |`}</span>)}
+              </motion.div >
+            )
+          })}
           <motion.h1 variants={itemVariants}>
             ...
-          </motion.h1>
-        </motion.div>
-        <motion.h1 className="font-extrabold" variants={itemVariants}>
-          .backStory
-        </motion.h1>
-        <motion.div className="grid pl-5">
-          <motion.h1 variants={itemVariants}>
-            {backStory}
           </motion.h1>
         </motion.div>
       </motion.div>
       <div className="inventory hidden xl:col-[3/-1] xl:row-[1/2] xl:z-50 xl:p-10 xl:block">
         <motion.h1 className="font-extrabold" variants={itemVariants}>
+          .backStory
+        </motion.h1>
+        <motion.div className="grid pl-5">
+          <motion.h1 variants={itemVariants}>
+            {content.backstory}
+          </motion.h1>
+        </motion.div>
+        <motion.h1 className="font-extrabold" variants={itemVariants}>
           .likes
         </motion.h1>
-        <motion.div className="grid pl-5 font-bold" variants={itemVariants}>
-          <motion.h1 variants={itemVariants}>
-            OS: fedora, android
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            genre: sci/fi, adventure, fantasy
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            music: anything
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            movie: the one that doesn&apos;t make me sleepy
-          </motion.h1>
-          <motion.h1 variants={itemVariants}>
-            person: Irene
-          </motion.h1>
+        <motion.div className="grid pl-5" variants={itemVariants}>
+          {Object.entries(content.likes).map((v, i) => {
+            return (
+              <motion.h1 key={i} variants={itemVariants}>
+                <span className='font-bold'>{v[0]}:</span>{v[1].map((v, i) => <span key={i}>{` ${v} |`}</span>)}
+              </motion.h1>
+            )
+          })}
         </motion.div>
       </div>
       <motion.div className="inventory hidden xl:col-[3/-1] xl:row-[3/4] xl:z-50 xl:p-10 xl:block">
@@ -201,7 +207,7 @@ export const IntroductionComponent = () => {
           .links
         </motion.h1>
         <ul className='grid pl-5'>
-          {Object.entries(links).map((v, i) => {
+          {Object.entries(content.links).map((v, i) => {
             return (
               <motion.li key={i} variants={itemVariants}>
                 <Link target="_blank" rel="noopener noreferrer" href={v[1]}>
