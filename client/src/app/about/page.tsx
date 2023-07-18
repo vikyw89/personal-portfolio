2a703de4c635da6aea9aedd4371cf0eec5a15257
vikyw89 <vikyw89@gmail.com>
2023-07-18 14:43:49 +0800

    fix: readjust about page to be centered, added backstory on mobile to fill empty space

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -4,7 +4,7 @@ import { IntroductionComponent } from './introduction'
 
 export default function Page() {
   return (
-    <div className="content grid text-base-content z-0 overflow-hidden items-center">
+    <div className="content grid text-base-content z-0 overflow-hidden items-center h-full">
       <IntroductionComponent />
     </div>
   )
