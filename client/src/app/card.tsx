ff11ce81ce2afac5c35888fec5506d297a37ea4c
vikyw89 <vikyw89@gmail.com>
2023-07-20 11:58:58 +0800

    perf: removed entry animation on card while scrolling

--- client/src/app/card.tsx
+++ client/src/app/card.tsx
@@ -3,7 +3,7 @@
 
 import ExitToAppIcon from '@mui/icons-material/ExitToApp'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import { motion, useAnimate, useInView } from 'framer-motion'
+import { motion, useInView } from 'framer-motion'
 import { useEffect, useRef } from 'react'
 
 export type CardProps = {
@@ -21,10 +21,7 @@ export type CardProps = {
 
 export const Card = ({ props }: CardProps) => {
   const video = useRef(null) as any
-  const [card, animate] = useAnimate()
-  const cardIsInView = useInView(card, { amount: 0.1 })
   const videoIsInView = useInView(video, { margin: '-40%' })
-  // const [watchVideo, setWatchVideo] = useState(false)
 
   useEffect(() => {
     if (!videoIsInView) {
@@ -38,28 +35,10 @@ export const Card = ({ props }: CardProps) => {
     video.current.play()
   }
 
-  useEffect(() => {
-    const entryPosition = props.index % 2 ? '50vw' : '-50vw'
-    if (!video.current) return
-    if (cardIsInView) {
-      animate(card.current,
-        {
-          x: 0,
-          opacity: 1,
-        })
-    } else {
-      animate(card.current,
-        {
-          x: entryPosition,
-          opacity: 0,
-        })
-    }
-  }, [card, cardIsInView, animate, props.index])
-
   return (
     <>
       {/* {watchVideo && <Modal props={{...props, setWatchVideo}}/>} */}
-      <div ref={card} onClick={openVideo} className="aspect w-full z-20 flex flex-col shadow-lg text-base-content sm:max-w-7xl sm:grid sm:grid-flow-dense sm:grid-cols-2 sm:gap-2 sm:backdrop-blur-sm sm:bg-secondary sm:text-secondary-content sm:bg-opacity-80 sm:rounded-xl">
+      <div onClick={openVideo} className="aspect w-full z-20 flex flex-col shadow-lg text-base-content sm:max-w-7xl sm:grid sm:grid-flow-dense sm:grid-cols-2 sm:gap-2 sm:backdrop-blur-sm sm:bg-secondary sm:text-secondary-content sm:bg-opacity-80 sm:rounded-xl">
         <div className="relative w-full">
           <video ref={video} className="aspect-square bg-primary bg-opacity-50 backdrop-blur-sm w-full shadow-2xl object-cover sm:aspect-square md:aspect-video" muted loop>
             <source src={props.videoSrc} type="video/webm" />
