b16052d02153b21b0510a9875b31dbd42b7267c8
vikyw89 <vikyw89@gmail.com>
2023-07-20 13:21:52 +0800

    perf: uses image and data placeholder on mobile, video on desktop to improve mobile performance

--- client/src/app/card.tsx
+++ client/src/app/card.tsx
@@ -3,7 +3,8 @@
 
 import ExitToAppIcon from '@mui/icons-material/ExitToApp'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import { motion, useInView } from 'framer-motion'
+import { motion, useAnimate, useInView } from 'framer-motion'
+import Image from 'next/image'
 import { useEffect, useRef } from 'react'
 
 export type CardProps = {
@@ -16,33 +17,69 @@ export type CardProps = {
     description: string,
     liveLink: string,
     repoLink: string,
+    imageSrc: string,
+    blurDataURL: string
   }
 }
 
 export const Card = ({ props }: CardProps) => {
   const video = useRef(null) as any
+  const [card, animate] = useAnimate()
+  const cardIsInView = useInView(card, { amount: 0.1 })
   const videoIsInView = useInView(video, { margin: '-40%' })
 
   useEffect(() => {
-    if (!videoIsInView) {
+    if (!videoIsInView && video.current) {
       video.current.pause()
     }
   }, [videoIsInView])
 
   const openVideo = (e: any) => {
+    if (!video.current) return
     e.stopPropagation()
     // setWatchVideo(true)
     video.current.play()
   }
 
+  useEffect(() => {
+    const entryPosition = props.index % 2 ? '50vw' : '-50vw'
+    if (cardIsInView) {
+      animate(card.current,
+        {
+          x: 0,
+          opacity: 1,
+        })
+    } else {
+      animate(card.current,
+        {
+          x: entryPosition,
+          opacity: 0,
+        })
+    }
+  }, [card, cardIsInView, animate, props.index])
+
   return (
     <>
       {/* {watchVideo && <Modal props={{...props, setWatchVideo}}/>} */}
-      <div onClick={openVideo} className="aspect w-full z-20 flex flex-col shadow-lg text-base-content sm:max-w-7xl sm:grid sm:grid-flow-dense sm:grid-cols-2 sm:gap-2 sm:backdrop-blur-sm sm:bg-secondary sm:text-secondary-content sm:bg-opacity-80 sm:rounded-xl">
+      <div ref={card} onClick={openVideo} className="aspect w-full z-20 flex flex-col shadow-lg text-base-content sm:max-w-7xl sm:grid sm:grid-flow-dense sm:grid-cols-2 sm:gap-2 sm:backdrop-blur-sm sm:bg-secondary sm:text-secondary-content sm:bg-opacity-80 sm:rounded-xl">
         <div className="relative w-full">
-          <video ref={video} className="aspect-square bg-primary bg-opacity-50 backdrop-blur-sm w-full shadow-2xl object-cover sm:aspect-square md:aspect-video" muted loop>
+          <video ref={video} className="hidden aspect-square bg-primary bg-opacity-50 backdrop-blur-sm w-full shadow-2xl object-cover sm:block sm:aspect-square md:aspect-video" muted loop>
             <source src={props.videoSrc} type="video/webm" />
           </video>
+          <Image
+            src={props.imageSrc}
+            alt={props.title}
+            width={1000}
+            height={1000}
+            sizes='1000px'
+            priority={true}
+            placeholder='blur'
+            blurDataURL={props.blurDataURL}
+            style={{
+              objectFit: 'cover'
+            }}
+            className='sm:hidden'
+          />
           <motion.div className="links inset-0 absolute grid grid-flow-col items-end  font-extrabold "
             initial={{
               opacity: 0,
